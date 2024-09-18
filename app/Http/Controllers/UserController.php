c4395dbb8c2fead513885c1440695821d676f9da
Fafayasinta <yasintafardiyani@gmail.com>
2024-09-19 01:12:55 +0700

    Jobsheet 4 - Praktikum-2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -43,9 +43,18 @@ class UserController extends Controller
         // });
         // return view('user', ['data' => $user]);
 
-        $user = UserModel::findor(20, ['username', 'nama'], function () {
-            abort(404);
-        });
+        // $user = UserModel::findor(20, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+
+
+        // jobsheet 4 prak 2.2
+        // $user = UserModel::findOrFail(1);
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
+
     }
 }
