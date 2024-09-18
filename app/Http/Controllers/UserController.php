6f27f87511520ebac7f83bcfcc43b9e2867c307c
Fafayasinta <yasintafardiyani@gmail.com>
2024-09-19 00:58:39 +0700

    Jobsheet 4 - Praktikum-2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,14 +9,14 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        // tambah data menggunakan Eloquent
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::insert($data);
+        // tambah data menggunakan Eloquent Model
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        // ];
+        // UserModel::insert($data);
 
         //update data dengan Eloquent
         // $data = [
@@ -25,7 +25,27 @@ class UserController extends Controller
         // UserModel::where('username', 'customer 1')->update($data); //update data user
 
         // ambil semua data dari model UserModel
-        $user = UserModel::all();
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        // jobsheet 4 prak 2.1
+        // $user = UserModel::find(1);
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::where('level_id', 1)->first();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::findor(1, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::findor(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,25 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Data user</title>
-</head>
-<body>
-    <h1>Data user</h1>
-    <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <td>ID</td>
-            <td>Username</td>
-            <td>Nama</td>
-            <td>ID Level pengguna</td>
-        </tr>
-        @foreach ($data as $d)
+<html>
+    <head>
+        <title>
+            Data User
+        </title>
+    </head>
+    <body>
+        <h1>
+            Data User
+        </h1>
+        <table border="1" cellpadding="2" cellspacing="0">
             <tr>
-                <td> {{$d->user_id}} </td>
-                <td> {{$d->username}} </td>
-                <td> {{$d->nama}} </td>
-                <td> {{$d->level_id}} </td>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Nama</th>
+                <th>ID Level Pengguna</th>
             </tr>
-        @endforeach
-    </table>
-</body>
+
+            <tr>
+                <td>{{ $data->user_id}}</td>
+                <td>{{ $data->username}}</td>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->level_id }}</td>
+                </tr>
+        </table>
+    </body>
 </html>
