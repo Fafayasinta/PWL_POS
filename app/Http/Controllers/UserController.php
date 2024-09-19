6a8ddfa09cdd4d9a34d0094fc888c9246f063b12
Fafayasinta <yasintafardiyani@gmail.com>
2024-09-19 19:01:39 +0700

    Jobsheet 4 - Praktikum-2.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -48,12 +48,16 @@ class UserController extends Controller
         // });
         // return view('user', ['data' => $user]);
 
-
         // jobsheet 4 prak 2.2
         // $user = UserModel::findOrFail(1);
         // return view('user', ['data' => $user]);
 
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // return view('user', ['data' => $user]);
+
+        // Jobsheet 4 prak 2.3
+        $user = UserModel::where('level_id', 2)->count();
+        // dd($user);
         return view('user', ['data' => $user]);
 
     }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -11,6 +11,9 @@
         </h1>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
+                <th>Jumlah Pengguna</th>
+            </tr>
+            {{-- <tr>
                 <th>ID</th>
                 <th>Username</th>
                 <th>Nama</th>
@@ -22,7 +25,10 @@
                 <td>{{ $data->username}}</td>
                 <td>{{ $data->nama }}</td>
                 <td>{{ $data->level_id }}</td>
-                </tr>
+            </tr> --}}
+            <tr>
+                <td>{{ $data }}</td>
+            </tr>
         </table>
     </body>
 </html>
