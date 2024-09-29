40e30f417143f75d0c0d35c7fd2b8f7cceb386d8
Fafayasinta <yasintafardiyani@gmail.com>
2024-09-29 14:01:37 +0700

    Jobsheet 5 - Tugas

--- app/Models/LevelModel.php
+++ app/Models/LevelModel.php
@@ -8,13 +8,20 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 
-class LevelModel extends Model{
-    use HasFactory;
-    protected $table = "m_level";
-    protected $primaryKey ="level_id";
-    public function user(): HasMany
-    {
-        return $this->hasMany(User::class);
-    }
-
+class LevelModel extends Model
+{
+    protected $table = 'm_level';        // Mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'level_id';  //Mendefinisikan primary key dari tabel yang digunakan
+    
+    protected $fillable = ['level_kode', 'level_nama'];
 }
+
+// class LevelModel extends Model{
+//     use HasFactory;
+//     protected $table = "m_level";
+//     protected $primaryKey ="level_id";
+//     public function user(): HasMany
+//     {
+//         return $this->hasMany(User::class);
+//     }
+
