3f4508e13d03d7db8cb466f4b02269b2faa08ac3
Fafayasinta <yasintafardiyani@gmail.com>
2024-10-04 10:50:12 +0700

    Jobsheet 6 - Praktikum-1

--- resources/views/user/create_ajax.blade.php
+++ resources/views/user/create_ajax.blade.php
@@ -10,7 +10,7 @@
             </div>
             <div class="modal-body">
                 <div class="form-group">
-                    <label for="level_id">Level Pengguna</label>
+                    <label>Level Pengguna</label>
                     <select name="level_id" id="level_id" class="form-control" required>
                         <option value="">- Pilih Level -</option>
                         @foreach($level as $l)
@@ -19,26 +19,22 @@
                     </select>
                     <small id="error-level_id" class="error-text form-text text-danger"></small>
                 </div>
-
                 <div class="form-group">
-                    <label for="username">Username</label>
-                    <input type="text" name="username" id="username" class="form-control" required>
+                    <label>Username</label>
+                    <input value="" type="text" name="username" id="username" class="form-control" required>
                     <small id="error-username" class="error-text form-text text-danger"></small>
                 </div>
-
                 <div class="form-group">
-                    <label for="nama">Nama</label>
-                    <input type="text" name="nama" id="nama" class="form-control" required>
+                    <label>Nama</label>
+                    <input value="" type="text" name="nama" id="nama" class="form-control" required>
                     <small id="error-nama" class="error-text form-text text-danger"></small>
                 </div>
-
                 <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" name="password" id="password" class="form-control" required>
+                    <label>Password</label>
+                    <input value="" type="password" name="password" id="password" class="form-control" required>
                     <small id="error-password" class="error-text form-text text-danger"></small>
                 </div>
             </div>
-
             <div class="modal-footer">
                 <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button>
                 <button type="submit" class="btn btn-primary">Simpan</button>
@@ -51,25 +47,10 @@
     $(document).ready(function() {
         $("#form-tambah").validate({
             rules: {
-                level_id: {
-                    required: true,
-                    number: true
-                },
-                username: {
-                    required: true,
-                    minlength: 3,
-                    maxlength: 20
-                },
-                nama: {
-                    required: true,
-                    minlength: 3,
-                    maxlength: 100
-                },
-                password: {
-                    required: true,
-                    minlength: 6,
-                    maxlength: 20
-                }
+                level_id: { required: true, number: true },
+                username: { required: true, minlength: 3, maxlength: 20 },
+                nama: { required: true, minlength: 3, maxlength: 100 },
+                password: { required: true, minlength: 6, maxlength: 20 }
             },
             submitHandler: function(form) {
                 $.ajax({
