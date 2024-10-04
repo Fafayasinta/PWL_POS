6cbea1e7d7ec3872c41b861e4c0a1efa5a8dfae4
Fafayasinta <yasintafardiyani@gmail.com>
2024-10-04 10:53:24 +0700

    Jobsheet 6 - Tugas

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -1,11 +1,12 @@
-@extends('layouts.template') 
- 
+@extends('layouts.template')
 @section('content')
+<div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
     <div class="card card-outline card-primary">
         <div class="card-header">
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
                 <a class="btn btn-sm btn-primary mt-1" href="{{ url('kategori/create') }}">Tambah</a>
+                <button onclick="modalAction('{{ url('/kategori/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
             </div>
         </div>
         <div class="card-body">
@@ -15,35 +16,62 @@
             @if (session('error'))
                 <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
+            {{-- tidak menggunakan filter untuk kategori --}}
+            {{-- <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">Filter:</label>
+                        <div class="col-3">
+                            <select class="form-control" id="kategori_id" name="kategori_id" required>
+                                <option value="">- Semua -</option>
+                                @foreach ($kategori as $item)
+                                    <option value="{{ $item->kategori_id }}">{{ $item->kategori_nama }}</option>
+                                @endforeach
+                            </select>
+                            <small class="form-text text-muted">kategori Pengguna</small>
+                        </div>
+                    </div>
+                </div>
+            </div> --}}
+            <table class="table table-bordered table-striped table-hover table-sm" id="table_kategori">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Kode</th>
+                        <th>Nama Kategori</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+            </table>
         </div>
-        <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Kategori Kode</th>
-                    <th>Kategori Nama</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-        </table>
-    </div>
     </div>
 @endsection
 @push('css')
 @endpush
 @push('js')
     <script>
+        function modalAction(url = '') {
+            $('#myModal').load(url, function() {
+                $('#myModal').modal('show');
+            });
+        }
+
+        var dataKategori;
         $(document).ready(function() {
-            var dataUser = $('#table_user').DataTable({
-                // serverSide: true, jika ingin menggunakan server side processing 
+            dataKategori = $('#table_kategori').DataTable({
+                // serverSide: true, jika ingin menggunakan server side processing
                 serverSide: true,
                 ajax: {
                     "url": "{{ url('kategori/list') }}",
                     "dataType": "json",
-                    "type": "POST",
+                    "type": "POST"
+                    // tidak perlu data dibawah karena tidak ada filter
+                    // "data": function (d) {
+                    //     d.kategori_id = $('#kategori_id').val();
+                    // }
                 },
                 columns: [{
-                    // nomor urut dari laravel datatable addIndexColumn() 
+                    // nomor urut dari laravel datatable addIndexColumn()
                     data: "DT_RowIndex",
                     className: "text-center",
                     orderable: false,
@@ -51,9 +79,9 @@
                 }, {
                     data: "kategori_kode",
                     className: "",
-                    // orderable: true, jika ingin kolom ini bisa diurutkan  
+                    // orderable: true, jika ingin kolom ini bisa diurutkan
                     orderable: true,
-                    // searchable: true, jika ingin kolom ini bisa dicari 
+                    // searchable: true, jika ingin kolom ini bisa dicari
                     searchable: true
                 }, {
                     data: "kategori_nama",
@@ -67,6 +95,10 @@
                     searchable: false
                 }]
             });
+
+            // $('#kategori_id').on('change',function(){
+            //     dataKategori.ajax.reload();
+            // });
         });
     </script>
 @endpush
