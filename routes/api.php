369e090902cbd92e54bc1d412b62258416df812b
Fafayasinta <yasintafardiyani@gmail.com>
2024-11-03 23:30:38 +0700

    Jobsheet 10 - Praktikum-3

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,7 @@ Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('lo
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-// Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 
 // // level
 // Route::get('level', [LevelController::class, 'index']);
